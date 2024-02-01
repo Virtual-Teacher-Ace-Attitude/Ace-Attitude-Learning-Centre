cc67aa4aad86398dfb056612c8b1775a8dcec90a
derpspy <aleks.arabadjiev05@gmail.com>
2024-02-01 23:50:08 +0200

    Add RegisterDTO. Created ModelMapper. Created ValidationConstraints project.

---
+++ AceAttitude.Services.Mapping/Contracts/IModelMapper.cs
@@ -0,0 +1,10 @@
+﻿using AceAttitude.Data.Models;
+using AceAttitude.Web.DTO.Request;
+
+namespace AceAttitude.Services.Mapping.Contracts
+{
+    public interface IModelMapper
+    {
+        public ApplicationUser MapToUser(UserRegisterRequestDTO userDTO, string passwordHash);
+    }
+}
---
+++ AceAttitude.Services.Mapping/ModelMapper.cs
@@ -0,0 +1,21 @@
+﻿using AceAttitude.Data.Models;
+using AceAttitude.Services.Mapping.Contracts;
+using AceAttitude.Web.DTO.Request;
+
+namespace AceAttitude.Services.Mapping
+{
+    public class ModelMapper : IModelMapper
+    {
+        public ApplicationUser MapToUser(UserRegisterRequestDTO userDTO, string passwordHash)
+        {
+            return new ApplicationUser
+            {
+                FirstName = userDTO.FirstName,
+                LastName = userDTO.LastName,
+                Email = userDTO.Email,
+                PasswordHash = passwordHash,
+                CreatedOn = DateTime.Now
+            };
+        }
+    }
+}
