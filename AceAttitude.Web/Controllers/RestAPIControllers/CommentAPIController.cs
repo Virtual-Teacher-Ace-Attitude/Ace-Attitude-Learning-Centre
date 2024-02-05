b512558adc68d9a149d1bb8759c98e71ff6f285f
alexkalionski <alexkalionski@gmail.com>
2024-02-05 19:46:47 +0200

    Added exception handling to Comment API controller.

--- AceAttitude.Web/Controllers/RestAPIControllers/CommentAPIController.cs
+++ AceAttitude.Web/Controllers/RestAPIControllers/CommentAPIController.cs
@@ -1,4 +1,5 @@
-﻿using AceAttitude.Data.Models;
+﻿using AceAttitude.Common.Exceptions;
+using AceAttitude.Data.Models;
 using AceAttitude.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +9,13 @@ namespace AceAttitude.Web.Controllers.RestAPIControllers
     [Route("api/courses/{courseId}/comments")]
     public class CommentAPIController : ControllerBase
     {
-        private readonly IUserService userService;
+        private readonly IAuthService authService;
         private readonly ICommentService commentService;
         private readonly ICourseService courseService;
 
-        public CommentAPIController(IUserService userService, ICommentService commentService, ICourseService courseService)
+        public CommentAPIController(IAuthService authService, ICommentService commentService, ICourseService courseService)
         {
-            this.userService = userService;
+            this.authService = authService;
             this.commentService = commentService;
             this.courseService = courseService;
         }
@@ -22,42 +23,96 @@ namespace AceAttitude.Web.Controllers.RestAPIControllers
         [HttpGet("")]
         public IActionResult GetComments(int courseId)
         {
-            var course = courseService.GetById(courseId);
-            List<Comment> comments = commentService.GetComments(course);
-            return Ok(comments);
+            try
+            {
+                var course = courseService.GetById(courseId);
+                List<Comment> comments = commentService.GetComments(course);
+                return Ok(comments);
+            }
+            catch (EntityNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+
         }
 
         [HttpPost("")]
-        public IActionResult PostComment(int courseId, [FromBody] Comment comment, [FromHeader] string userId)
+        public IActionResult PostComment(int courseId, [FromBody] Comment comment, [FromHeader] string credentials)
         {
-            var user = userService.GetById(userId);
-            var course = courseService.GetById(courseId);
-            Comment createdComment = commentService.CreateComment(comment, course, user);
-            return StatusCode(StatusCodes.Status201Created, createdComment);
+            try
+            {
+                var user = authService.TryGetUser(credentials);
+                var course = courseService.GetById(courseId);
+                Comment createdComment = commentService.CreateComment(comment, course, user);
+                return StatusCode(StatusCodes.Status201Created, createdComment);
+            }
+            catch (EntityNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (UnauthorizedOperationException e)
+            {
+                return Unauthorized(e.Message);
+            }
+
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeleteComment(int id, [FromHeader] string userId)
+        public IActionResult DeleteComment(int id, [FromHeader] string credentials)
         {
-            var user = userService.GetById(userId);
-            var deletedComment = commentService.DeleteComment(id, user);
-            return Ok(deletedComment);
+            try
+            {
+                var user = authService.TryGetUser(credentials);
+                var deletedComment = commentService.DeleteComment(id, user);
+                return Ok(deletedComment);
+            }
+            catch (EntityNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (UnauthorizedOperationException e)
+            {
+                return Unauthorized(e.Message);
+            }
+
         }
 
         [HttpPost("{id}")]
-        public IActionResult EditComment(int id, [FromBody] string content, [FromHeader] string userId)
+        public IActionResult EditComment(int id, [FromBody] string content, [FromHeader] string credentials)
         {
-            var user = userService.GetById(userId);
-            var updatedComment = commentService.UpdateComment(id, content, user);
-            return Ok(updatedComment);
+            try
+            {
+                var user = authService.TryGetUser(credentials);
+                var updatedComment = commentService.UpdateComment(id, content, user);
+                return Ok(updatedComment);
+            }
+            catch (EntityNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (UnauthorizedOperationException e)
+            {
+                return Unauthorized(e.Message);
+            }
         }
 
         [HttpPut("{id}")]
-        public IActionResult LikeComment(int id, [FromHeader] string userId)
+        public IActionResult LikeComment(int id, [FromHeader] string credentials)
         {
-            var user = userService.GetById(userId);
-            var comment = commentService.LikeComment(id, user);
-            return Ok(comment);
+            try
+            {
+                var user = authService.TryGetUser(credentials);
+                var comment = commentService.LikeComment(id, user);
+                return Ok(comment);
+            }
+            catch (EntityNotFoundException e)
+            {
+                return NotFound(e.Message);
+            }
+            catch (UnauthorizedOperationException e)
+            {
+                return Unauthorized(e.Message);
+            }
         }
     }
 }
