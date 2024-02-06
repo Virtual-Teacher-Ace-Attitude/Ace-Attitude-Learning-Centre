7f2cd93de7b0485a5959b081ddfa0a9e522714b1
alexkalionski <alexkalionski@gmail.com>
2024-02-06 17:37:28 +0200

    Added authHelper class to Common folder (project Helpers). Updated LectureService.

---
+++ Helpers/AuthHelper.cs
@@ -0,0 +1,47 @@
+﻿using AceAttitude.Common.Exceptions;
+using AceAttitude.Data.Models;
+
+namespace Helpers
+{
+    public class AuthHelper
+    {
+        private const string StudentNotEnrolledErrorMessage = "You are not enrolled in this course!";
+        private const string TeacherNotApprovedErrorMessage = "You are not an approved teacher!";
+        private const string TeacherNotCourseCreator = "You are not the creator of this course!";
+        public void EnsureTeacherIsCourseCreator(Teacher teacher, int courseId)
+        {
+            if (!teacher.CreatedCourses.Any(cc => cc.Id == courseId))
+            {
+                throw new UnauthorizedOperationException(TeacherNotCourseCreator);
+            }
+        }
+
+        public void EnsureStudentEnrolled(Student student, int courseId)
+        {
+            if (!student.StudentCourses.Any(sc => sc.CourseId == courseId))
+            {
+                throw new UnauthorizedOperationException(StudentNotEnrolledErrorMessage);
+            }
+        }
+
+        public void EnsureTeacherApproved(Teacher teacher)
+        {
+            if (teacher.IsApproved == false)
+            {
+                throw new UnauthorizedOperationException(TeacherNotApprovedErrorMessage);
+            }
+        }
+
+        public string ReturnUserType(ApplicationUser user)
+        {
+            if (user.TeacherId is null)
+            {
+                return "student";
+            }
+            else
+            {
+                return "teacher";
+            }
+        }
+    }
+}
