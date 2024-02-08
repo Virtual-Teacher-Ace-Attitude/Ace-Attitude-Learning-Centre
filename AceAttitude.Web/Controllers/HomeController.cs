e3a38ffa0c0b283b0fc34ef081ff17a2384a7e5d
derpspy <aleks.arabadjiev05@gmail.com>
2024-02-08 22:24:30 +0200

    Attempt to fix merge disaster.

--- AceAttitude.Web/Controllers/HomeController.cs
+++ AceAttitude.Web/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace AceAttitude.Controllers
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         //public IActionResult Error()
         //{
-        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier  });
         //}
     }
 }
