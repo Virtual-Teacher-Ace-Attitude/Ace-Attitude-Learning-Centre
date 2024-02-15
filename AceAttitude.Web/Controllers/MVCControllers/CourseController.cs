5ac62903a2e612e2793865bbb569118bc2d7ca3c
alexkalionski <alexkalionski@gmail.com>
2024-02-15 13:15:59 +0200

    Fixed details view for course controller.

--- AceAttitude.Web/Controllers/MVCControllers/CourseController.cs
+++ AceAttitude.Web/Controllers/MVCControllers/CourseController.cs
@@ -4,19 +4,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AceAttitude.Web.Controllers.MVCControllers
 {
+    [Route("courses")]
     public class CourseController : Controller
     {
         private readonly ICourseService courseService;
 
+        
         public CourseController(ICourseService courseService)
         {
             this.courseService = courseService;
         }
-        public IActionResult Index([FromQuery]string filterParam, [FromQuery]string filterParamValue, [FromQuery] string sortParam)
+        [HttpGet("")]
+        public IActionResult Index([FromQuery] string filterParam, [FromQuery] string filterParamValue, [FromQuery] string sortParam)
         {
             List<Course> courses = courseService.GetAll(filterParam, filterParamValue, sortParam);
             return View(courses);
         }
+
+        [HttpGet("{id}")]
         public IActionResult Details([FromRoute] int id) 
         {
             Course course = courseService.GetById(id);
